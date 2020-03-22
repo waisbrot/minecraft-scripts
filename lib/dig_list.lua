07d0625590e5e45585b920fab60530b7d5af7ac4
Nathaniel Waisbrot <nwaisbrot@fitbit.com>
2020-03-21 21:17:28 -0400

    fix array init syntax

--- lib/dig_list.lua
+++ lib/dig_list.lua
@@ -1,24 +1,24 @@
 SAFE = {
-  "minecraft:stone" = true
-  "minecraft:dirt" = true
-  "minecraft:grass" = true
-  "minecraft:sand" = true
-  "minecraft:gravel" = true
-  "minecraft:lapis_ore" = true
-  "minecraft:sandstone" = true
-  "minecraft:web" = true
-  "minecraft:tallgrass" = true
-  "minecraft:deadbush" = true
-  "minecraft:diamond_ore" = true
-  "minecraft:redstone_ore" = true
-  "minecraft:lit_redstone_ore" = true
-  "minecraft:snow_layer" = true
-  "minecraft:ice" = true
-  "minecraft:snow" = true
-  "minecraft:clay" = true
-  "minecraft:netherrack" = true
-  "minecraft:soul_sand" = true
-  "minecraft:glowstone" = true
+  ["minecraft:stone"] = true,
+  ["minecraft:dirt"] = true,
+  ["minecraft:grass"] = true,
+  ["minecraft:sand"] = true,
+  ["minecraft:gravel"] = true,
+  ["minecraft:lapis_ore"] = true,
+  ["minecraft:sandstone"] = true,
+  ["minecraft:web"] = true,
+  ["minecraft:tallgrass"] = true,
+  ["minecraft:deadbush"] = true,
+  ["minecraft:diamond_ore"] = true,
+  ["minecraft:redstone_ore"] = true,
+  ["minecraft:lit_redstone_ore"] = true,
+  ["minecraft:snow_layer"] = true,
+  ["minecraft:ice"] = true,
+  ["minecraft:snow"] = true,
+  ["minecraft:clay"] = true,
+  ["minecraft:netherrack"] = true,
+  ["minecraft:soul_sand"] = true,
+  ["minecraft:glowstone"] = true,
 }
 
 -- Everything not listed above is unsafe!
